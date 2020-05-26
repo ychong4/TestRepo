8f588046e7a9d1bcb0884a60fa51acfd2d43bac1
ychong4 <65525074+ychong4@users.noreply.github.com>
2020-05-26 00:55:55 -0500

    assign2_backup

---
+++ mfscrm_back_up/crm/views.py
@@ -0,0 +1,80 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from .models import *
+from .forms import *
+from django.shortcuts import redirect
+
+now = timezone.now()
+def home(request):
+   return render(request, 'crm/home.html',
+                 {'crm': home})
+
+
+@login_required
+def customer_list(request):
+    customer = Customer.objects.filter(created_date__lte=timezone.now())
+    return render(request, 'crm/customer_list.html',
+                 {'customers': customer})
+
+@login_required
+def customer_edit(request, pk):
+   customer = get_object_or_404(Customer, pk=pk)
+   if request.method == "POST":
+       # update
+       form = CustomerForm(request.POST, instance=customer)
+       if form.is_valid():
+           customer = form.save(commit=False)
+           customer.updated_date = timezone.now()
+           customer.save()
+           customer = Customer.objects.filter(created_date__lte=timezone.now())
+           return render(request, 'crm/customer_list.html',
+                         {'customers': customer})
+   else:
+        # edit
+       form = CustomerForm(instance=customer)
+   return render(request, 'crm/customer_edit.html', {'form': form})
+
+@login_required
+def customer_delete(request, pk):
+   customer = get_object_or_404(Customer, pk=pk)
+   customer.delete()
+   return redirect('crm:customer_list')
+
+@login_required
+def service_list(request):
+   services = Service.objects.filter(created_date__lte=timezone.now())
+   return render(request, 'crm/service_list.html', {'services': services})
+
+@login_required
+def service_new(request):
+   if request.method == "POST":
+       form = ServiceForm(request.POST)
+       if form.is_valid():
+           service = form.save(commit=False)
+           service.created_date = timezone.now()
+           service.save()
+           services = Service.objects.filter(created_date__lte=timezone.now())
+           return render(request, 'crm/service_list.html',
+            {'services': services})
+   else:
+       form = ServiceForm()
+       # print("Else")
+   return render(request, 'crm/service_new.html', {'form': form})
+
+@login_required
+def service_edit(request, pk):
+   service = get_object_or_404(Service, pk=pk)
+   if request.method == "POST":
+       form = ServiceForm(request.POST, instance=service)
+       if form.is_valid():
+           service = form.save()
+           # service.customer = service.id
+           service.updated_date = timezone.now()
+           service.save()
+           services = Service.objects.filter(created_date__lte=timezone.now())
+           return render(request, 'crm/service_list.html', {'services': services})
+   else:
+       # print("else")
+       form = ServiceForm(instance=service)
+   return render(request, 'crm/service_edit.html', {'form': form})
+
