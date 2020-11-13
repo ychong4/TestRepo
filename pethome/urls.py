0a8e2d831d78e23898c500f614d01d3d76c828bd
ychong4 <65525074+ychong4@users.noreply.github.com>
2020-11-12 21:09:29 -0600

    Update urls.py

--- pethome/urls.py
+++ pethome/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     path('users/', include('django.contrib.auth.urls')),
 ]
 
-if settings.DEBUG:
+
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
